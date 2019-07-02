f3792a312351846217643fffff3044ab0da8bd2c
FionaStrausz <52449987+FionaStrausz@users.noreply.github.com>
2019-07-02 12:02:54 +0200

    Version 2
    The echobot has now more responses

--- Echobot.py
+++ Echobot.py
@@ -1,11 +1,21 @@
-import time
+import time,random,re
+
+responses = {"Hi":["Hello","Goodday","Hi"],
+             "Hello":["Hello","Goodday","Hi"],
+             "Goodday":["Hello","Goodday","Hi"],
+             "What is your name?":["My name is ALIE","ALIE","They call me ALIE"],
+             "How are you?":["Fine","Worse than you","I am doing great"]} 
 
 def respond(message):
-  return"Hello I am ALIE I am going to repeat everything you say: {}".format(message)
-
-#respond("Hello I am a duck")
+  if message in responses:
+    return random.choice(responses[message])
+  else: 
+    return"I don't know what to respond to: {}".format(message)
 
 def send_message(message):
   print("USER: {}".format(message))
-  time.sleep(0.4)
+  time.sleep(random.randint(1,3))
   print("ECHOBOT: {}".format(respond(message)))
+  
+while True:
+  send_message(input())
